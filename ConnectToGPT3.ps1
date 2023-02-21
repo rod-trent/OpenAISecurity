7a209fa0c0b4375cbaf5666531805f15c3bb9b39
Rod Trent <56966432+rod-trent@users.noreply.github.com>
2023-02-21 11:07:07 -0500

    Create ConnectToGPT3.ps1

---
+++ ConnectToGPT3.ps1
@@ -0,0 +1,29 @@
+//This PowerShell script connects to the GPT3 API, asks a question, and retrieves the answer.
+
+//Get your API key here: https://platform.openai.com/account/api-keys
+
+
+# Set your API key
+$apiKey = "YOUR_API_KEY"
+
+# Set the API endpoint URL
+$url = "https://api.openai.com/v1/engines/davinci-codex/completions"
+
+# Set the request headers
+$headers = @{
+    "Authorization" = "Bearer $apiKey"
+    "Content-Type" = "application/json"
+}
+
+# Set the request body
+$body = @{
+    "prompt" = "What is Microsoft Sentinel?"
+    "temperature" = 0.5
+    "max_tokens" = 100
+}
+
+# Send the HTTP request to the API endpoint
+$response = Invoke-RestMethod -Uri $url -Method Post -Headers $headers -Body ($body | ConvertTo-Json)
+
+# Print the response
+Write-Host $response.choices.text
